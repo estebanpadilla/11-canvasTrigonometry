44422fe734abf5a7348736a28ecb0faf299b8721
Esteban Padilla <ep@estebanpadilla.com>
2017-01-19 23:00:01 -0600

    Save

--- app.js
+++ app.js
@@ -16,28 +16,28 @@ function init() {
     let grid = Grid(Vector(0, 0), width, height, 20, AppColors().lineColor1, context);
     pool.push(grid);
 
-    let lineVert = Line(x, 0, x, height, 2, 'round', AppColors().lineColor2, context);
+    let lineVert = Line(Vector(x, 0), Vector(x, height), 2, 'round', AppColors().lineColor2, context);
     pool.push(lineVert);
 
-    let lineHorz = Line(0, y, width, y, 2, 'round', AppColors().lineColor2, context);
+    let lineHorz = Line(Vector(0, y), Vector(width, y), 2, 'round', AppColors().lineColor2, context);
     pool.push(lineHorz);
 
     let ball = Ball(Vector(x, y), 5, AppColors().myred, context);
     pool.push(ball);
 
-    let vector = Line(x, y, x, y, 3, 'round', AppColors().myred, context);
+    let vector = Line(Vector(x, y), Vector(x, y), 3, 'round', AppColors().red, context);
     pool.push(vector);
 
-    let xtext = new TextBox(20, 40, 25, 'x: 0', AppColors().xcolor, context);
+    let xtext = new Text(Vector(20, 40), 25, 'x: 0', AppColors().xcolor, context);
     pool.push(xtext);
 
-    let ytext = new TextBox(20, 65, 25, 'x: 0', AppColors().ycolor, context);
+    let ytext = new Text(Vector(20, 65), 25, 'x: 0', AppColors().ycolor, context);
     pool.push(ytext);
 
-    let ctext = new TextBox(20, 90, 25, 'cos: 0', AppColors().textColor, context);
+    let ctext = new Text(Vector(20, 90), 25, 'cos: 0', AppColors().textColor, context);
     pool.push(ctext);
 
-    let stext = new TextBox(20, 115, 25, 'sin: 0', AppColors().textColor, context);
+    let stext = new Text(Vector(20, 115), 25, 'sin: 0', AppColors().textColor, context);
     pool.push(stext);
 
     function clickHandler(e) {
@@ -47,13 +47,13 @@ function init() {
         let my = Math.round(e.clientY / 20) * 20;
 
         ball.position = Vector(mx, my);
-        vector.x2 = mx;
-        vector.y2 = my;
+        vector.p2.x = mx;
+        vector.p2.y = my;
 
         xtext.text = 'x: ' + (mx - x) / 20;
         ytext.text = 'y: ' + (my - y) / 20;
 
-        var angler = Vector(vector.x1, vector.y1).angleTo(Vector(vector.x2, vector.y2));
+        var angler = Vector(vector.p1.x, vector.p1.y).angleTo(Vector(vector.p2.x, vector.p2.y));
         //console.log(angle);
 
         ctext.text = 'cos: ' + Math.fround(Math.cos(angler));
